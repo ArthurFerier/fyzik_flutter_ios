7e30c8a0bf60e15d7943ec556d44f7409dba02df
ArthurFerier <arthur.ferier1@gmail.com>
2022-08-11 17:38:23 +0200

    application seems to be working like normally

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,111 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:stimulusep/db/database.dart';
+import 'package:stimulusep/model/exercise_enum.dart';
+import 'package:stimulusep/views/signup.dart';
+import 'package:stimulusep/views/tabs.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:package_info/package_info.dart';
+import 'package:stimulusep/views/verify_email_page.dart';
 
-void main() async {
+import 'model/exercise.dart';
+import 'model/report.dart';
+import 'model/user.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  //print(await addCodesInExt(doStuff()));
 
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  bool _existingUser = await FDatabase.instance.userExists();
+  if (_existingUser){
+    // check if user account is valid or disabled
+    SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    // for later upgrades
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    prefs.setString("app_version", packageInfo.version);
+
+    // if user is logged for more than a year
+    String? date = prefs.getString("dateLastLogin");
+    if (date == null) {
+      prefs.setString("dateLastLogin", DateTime.now().toString());
+    } else {
+      DateTime dateNow = DateTime.now();
+      DateTime dateBefore = DateTime.parse(date);
+      if (dateNow.difference(dateBefore).inDays >= 364) {
+        await databaseFactory.deleteDatabase('database.db');
+        UserC().deleteUser();
+
+        runApp(MaterialApp(
+          home: SignupPage(),
+          debugShowCheckedModeBanner: false,
+        ));
+        return;
+      }
+    }
+
+
+    await FDatabase.instance.getUser();
+
+    await FDatabase.instance.getAllReports().then(
+            (value) => value.forEach((element) {UserC().addReport(element, true);})
     );
+    for (Report report in UserC().reports){
+      List<Exercise> exercises = await FDatabase.instance.getAllExercisesFromReport(report.id);
+      exercises.forEach((element) {NameList.instance.testExercise(element);});
+      await report.initialiseExercises(exercises);
+    }
+
+    runApp(MaterialApp(
+        home: TabsPage(),
+        debugShowCheckedModeBanner: false,
+    ));
+  } else {
+    // there is no user in the database
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? uid = prefs.getString("info");
+    if(uid!=null){
+      List<String> list = uid.split(' ');
+      assert(list.length==3);
+      runApp(MaterialApp(
+          home: VerifyEmailPage(list[0], list[1], list[2]),
+          debugShowCheckedModeBanner: false,
+      ));
+    }else runApp(MaterialApp(
+        home: SignupPage(),
+        debugShowCheckedModeBanner: false,
+    ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+// not used
+Future<void> checkUserHasAllInfos() async {
+  try {
+    UserC().school!;
+  } catch (e) {
+    // signin out the user
+    await databaseFactory.deleteDatabase('database.db');
+    UserC().deleteUser();
+    runApp(MaterialApp(
+      home: SignupPage(),
+      debugShowCheckedModeBanner: false,
+    ));
   }
 }
+
+
+/*
+List<String> doStuff() {
+  List<String> list = text.split(' ');
+  return list;
+}
+
+String text = "EB6A-N8JI-21KM-E8E1 "+
+    "EB6A-JD04-FL1V-D1D1 "+
+    "EB6A-IGVR-J0RE-BAC1 "+
+    "EB6A-045O-NGZ9-A3B1 "+
+    "EB6A-DFT1-SED8-8CA1";
+ */
